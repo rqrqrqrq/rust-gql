c1f43b90afb9c22d9ada576b59e05b7338bd6449
rqrqrqrq <rqtdfh@gmail.com>
2018-03-22 21:05:05 +0300

    add gql helloworld

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,61 @@
 
 extern crate rocket;
 extern crate dotenv;
+#[macro_use]
+extern crate juniper;
+extern crate juniper_rocket;
 
 use dotenv::dotenv;
 use std::env;
+use rocket::response::content::Html;
+use rocket::State;
+use juniper::{RootNode, EmptyMutation};
+use juniper_rocket::{GraphQLResponse, GraphQLRequest, graphiql_source};
 
-#[get("/")]
-fn index() -> String {
-    env::var("DATABASE_URL").expect("`DATABASE_URL` should be in env vars")
+struct Context {
+    url: String,
+}
+
+impl juniper::Context for Context {}
+
+struct Query;
+
+graphql_object!(Query: Context |&self| {
+    field foo() -> String {
+        "Hello World".to_owned()
+    }
+
+    field url(&executor) -> String {
+        executor.context().url.clone()
+    }
+});
+
+type Schema = RootNode<'static, Query, EmptyMutation<Context>>;
+
+#[post("/graphql", data = "<request>")]
+fn graphql(
+    request: GraphQLRequest,
+    schema: State<Schema>,
+    context: State<Context>,
+) -> GraphQLResponse {
+    request.execute(&schema, &context)
+}
+
+#[get("/playground")]
+fn playground() -> Html<String> {
+    graphiql_source("/graphql")
 }
 
 fn main() {
     dotenv().expect("`.env` file is not present");
 
-    rocket::ignite().mount("/", routes![index]).launch();
+    let database_url = env::var("DATABASE_URL").expect("`DATABASE_URL` should be in env vars");
+
+    let context = Context { url: database_url };
+
+    rocket::ignite()
+        .manage(context)
+        .manage(Schema::new(Query, EmptyMutation::<Context>::new()))
+        .mount("/", routes![playground, graphql])
+        .launch();
 }
