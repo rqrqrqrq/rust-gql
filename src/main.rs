da740e17bba08887c1c37f7dcd2e343689b57c97
rqrqrqrq <rqtdfh@gmail.com>
2018-03-22 16:50:05 +0300

    add dotenv and database url

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+extern crate dotenv;
+
+use dotenv::dotenv;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    dotenv().expect("`.env` file is not present");
+
+    let database_url = env::var("DATABASE_URL").expect("`DATABASE_URL` should be in env vars");
+
+    println!("Hello, world! {:?}", database_url);
 }
