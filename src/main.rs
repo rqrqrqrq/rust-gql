8440fbf54b88c046e11396f9926628d426f7243d
rqrqrqrq <rqtdfh@gmail.com>
2018-03-22 19:40:15 +0300

    add rocket helloworld

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
+
+extern crate rocket;
 extern crate dotenv;
 
 use dotenv::dotenv;
 use std::env;
 
+#[get("/")]
+fn index() -> String {
+    env::var("DATABASE_URL").expect("`DATABASE_URL` should be in env vars")
+}
+
 fn main() {
     dotenv().expect("`.env` file is not present");
 
-    let database_url = env::var("DATABASE_URL").expect("`DATABASE_URL` should be in env vars");
-
-    println!("Hello, world! {:?}", database_url);
+    rocket::ignite().mount("/", routes![index]).launch();
 }
